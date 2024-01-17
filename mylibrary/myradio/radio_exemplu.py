7c9381974da26e1873ac23ce07f8bb3b5a07e25c
Rodica Brudea <rodiqha@yahoo.com>
2024-01-17 15:46:36 +0200

    Delete mylibrary directory

--- mylibrary/myradio/radio_exemplu.py
+++
@@ -1,6 +0,0 @@
-from pyradios import RadioBrowser
-
-
-def radio(*args):
-    rb = RadioBrowser()
-    print(rb.search(name="BBC Radio 1", name_exact=True))
--- mylibrary/userprofile/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from userprofile import views
-
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('adauga_melodie/', views.adauga_melodie, name='add'),
-]
